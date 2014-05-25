54fe345b3a6f722210d4db122408829decb1b1ff
ashmead <ashmead@users.noreply.github.com>
2014-05-25 00:12:40 -0400

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,22 +1,34 @@
 /*
-Name : main.c
-Disc : the most absolutely stunning hello world program in thello mutha fugin world!!!
-Auth : Ashmead Mohammed
+ * This software is free to use and distribute in any form the end user sees fit.
+ * The author will stand no liability for damage or loss incured prior, during  and after its use. 
+Name        : the most absolutely stunning hello world program in thello mutha fugin world!!!
+Author      : Ashmead Mohammed
+Discription : Hello World algorithm using abstract data types and some other basics in C. 
 */
 
+#include <stdio.h>
 #include "hello.h"
 
 int main (int argc, char **argv) {
-	
-	World *Whello = NewWorld(1, "Hello") ;
-	World *Wworld = NewWorld(2, "World") ;
-
-	Say(Whello) ;
-	Say(Wworld) ;
-
-	DestroyWorld(Whello) ;
-	DestroyWorld(Wworld) ;
-	
-	return 0 ;
-
+	if(argc == 1){
+		printf("Usage: %s arg1 arg2...", argv[0]) ;
+		return -1 ;
 	}
+
+	int i, j, k ;
+	Object *hello[argc-1] ;
+
+	for(i = 0 ; i < argc ; i++) {
+		hello[i] = NewObject(i, argv[i]) ;
+	}
+
+	for(j = 0 ; j < argc ; j++) {
+		ObjectOutput(hello[j]) ;
+	}
+	
+	for(k = 1 ; k < argc ; k++) {
+		DestroyObject(hello[k]) ;
+	}
+
+return 0 ;
+}
